85616e75e7d24b062313f888d618a90611e5dacc
Illia Budaiev <kurin899@gmail.com>
2020-01-13 19:33:39 +0200

    Домашнее задание №8 - Fibo
    Домашка сделана!

---
+++ src/HomeWork8_Fibo/MyFibo.java
@@ -0,0 +1,18 @@
+package HomeWork8_Fibo;
+
+public class MyFibo {
+    public static void main(String[] args) {
+
+
+        System.out.println(getFiboOf(35));
+
+    }
+
+    private static int getFiboOf(int index){
+        if(index==1 || index==2){
+            return 1;
+        }else {
+            return (getFiboOf(index-1) +getFiboOf(index-2));
+        }
+    }
+}
