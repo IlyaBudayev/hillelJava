8c3c78867abf7e6f60254e8bb74da94b0cd15746
Illia Budaiev <kurin899@gmail.com>
2020-01-13 20:55:35 +0200

    Домашнее задание №8 - Fibo
    Домашка сделана!

--- src/HomeWork8_Fibo/MyFibo.java
+++ src/HomeWork8_Fibo/MyFibo.java
@@ -1,10 +1,12 @@
 package HomeWork8_Fibo;
 
+import javafx.scene.chart.ScatterChart;
+
 public class MyFibo {
     public static void main(String[] args) {
 
 
-        System.out.println(getFiboOf(35));
+        System.out.println(calculateFib(35));
 
     }
 
@@ -14,5 +16,22 @@ public class MyFibo {
         }else {
             return (getFiboOf(index-1) +getFiboOf(index-2));
         }
+    }
+
+    public static long[] cache = new long[51];
+
+    public static long calculateFib(int ind){
+        if(cache[ind]!=0){
+            return cache[ind];
+        }else if(ind==0||ind==1||ind==2){
+             cache[ind]=1;
+            return 1;
+
+        }else if(ind>2) {
+            long resultat= (calculateFib(ind-1) +calculateFib(ind-2));
+            cache[ind]=resultat;
+            return cache[ind];
+        }
+        return cache[ind];
     }
 }
