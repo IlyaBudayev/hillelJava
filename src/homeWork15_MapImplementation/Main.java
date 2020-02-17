b0c77d891a7ea963ccce639557cc82463d08fa12
Illia Budaiev <kurin899@gmail.com>
2020-02-17 18:07:43 +0200

    Домашнее задание №15 - myMap

--- src/homeWork15_MapImplementation/Main.java
+++ src/homeWork15_MapImplementation/Main.java
@@ -1,17 +1,50 @@
 package homeWork15_MapImplementation;
 
 
-import homework14_Dictionary.MyMap;
 
 public class Main {
     public static void main(String[] args) {
         MyMap myMap = new MyMap();
-        myMap.add("Alphabet", "Алфавит");
-        myMap.add("Zepelin", "Дирижабль");
+
+        System.out.println(myMap.size());
+
+        myMap.put("Alphabet", "Алфавит");
+        myMap.put("Zepelin", "Дирижабль");
+
+
+        MyMap myMap2 = new MyMap();
+
+        System.out.println(myMap.size());
+
+        myMap2.put("Joint", "Сустав");
+        myMap2.put("Leverage", "Плечо");
+        myMap2.put("Pet", "Питомец");
+
+
+
 
 
         System.out.println(myMap);
-        String result = myMap.get("Alphabet");
+
+        System.out.println(myMap2);
+        String result = (String) myMap.get("Alphabet");
         System.out.println(result);
+
+        System.out.println(myMap.containsKey("Alphabet"));
+        System.out.println(myMap.containsKey("Zepelin"));
+        System.out.println(myMap.containsKey(11));
+
+        System.out.println(myMap.containsValue("Алфавит"));
+        System.out.println(myMap.containsValue("No!"));
+
+        System.out.println(myMap.values());
+        System.out.println(myMap.keySet());
+        System.out.println(myMap.entrySet());
+
+
+       // myMap.putAll(myMap2);
+        System.out.println(myMap);
+
+
     }
 }
---
+++ src/homeWork15_MapImplementation/MyMap.java
@@ -0,0 +1,255 @@
+package homeWork15_MapImplementation;
+
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+
+public class MyMap implements Map {
+    private int capacity = 10;
+    private Entry[] array = new Entry[capacity];
+
+
+    public MyMap() {
+    }
+
+    @Override
+    public int size() {
+        return array.length;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.length==0;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+    int hash = index(key);
+        if(size()>0&& size()>= index(key)) {
+            if (array[hash] != null) {
+                if (array[index(key)].key.equals(key)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+
+        if(size()>0){
+            for (Entry current: array
+                 ) {
+
+                if (current!=null) {
+                    if (current.value.equals(value)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Object get(Object key){
+
+        int hash  = index(key);
+        if(array[hash]!=null){
+            Entry current = array[hash];
+            while (true){
+                if(current.key.equals(key)){
+                    return current.value;
+                }
+                if(current == null){
+                    return null;
+                }
+                current = current.next;
+            }
+        }
+        return array[hash].value;
+    }
+
+
+    @Nullable
+    @Override
+    public Object put(Object key, Object value) {
+
+
+        Entry entry = new Entry(key, value);
+
+        int number = index(key);
+
+        add(entry, number);
+
+        return true;
+    }
+
+    public boolean add(Entry o,int index) {
+
+        int size = array.length+1;
+        Entry[] temp = new Entry[size];
+
+        if (size==0){
+            temp = new Entry[index+1];
+            temp[index]=o;
+            array=temp;
+            return true;
+        }
+        if(size<=index) {
+            temp = new Entry[index+1];
+
+        }
+
+
+        for (int i = 0; i <temp.length ; i++) {
+            if(i==index){
+                temp[index]=o;
+
+            }else if(array.length>i) {
+
+                temp[i] = array[i];
+            }else{
+                temp[i] = null;
+
+            }
+        }
+        array=temp;
+
+        return true;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(@NotNull Map m) {
+
+    }
+
+    @Override
+    public void clear() {
+        array = new Entry[capacity];
+    }
+
+    @NotNull
+    @Override
+    public Set keySet() {
+
+        ArrayList<Object> key = new ArrayList<>();
+
+        if(size()>0){
+            for (Entry current: array
+            ) {
+                if (current!=null) {
+                    key.add(current.key);
+                }
+            }
+        }
+
+        Set set =new HashSet(key);
+
+        return set;
+    }
+
+    @NotNull
+    @Override
+    public Collection values() {
+        ArrayList<Object> values = new ArrayList<>();
+
+        if(size()>0){
+            for (Entry current: array
+            ) {
+                if (current!=null) {
+                  values.add(current.value);
+                }
+            }
+        }
+
+        return values;
+    }
+
+    @NotNull
+    @Override
+    public Set<Map.Entry> entrySet() {
+
+        HashSet<Map.Entry> mySet = new HashSet<>();
+
+        for (Entry currEntry: array
+             ) {
+            mySet.add(currEntry);
+        }
+
+        return mySet;
+    }
+
+
+    private static class Entry implements Map.Entry {
+        Object key;
+        Object value;
+        Entry next;
+
+        public Entry(Object key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+
+
+        @Override
+        public Object getKey() {
+            return key;
+        }
+
+        @Override
+        public Object getValue() {
+            return value;
+        }
+
+        @Override
+        public Object setValue(Object value) {
+            return this.value=value;
+        }
+    }
+
+    private int index(Object s){
+
+        int hash = s.hashCode();
+
+        hash = Math.abs(hash);
+        hash = hash % capacity;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+
+        String head = "MyMap{" +
+                "capacity=" + capacity +
+                ", array=[";
+
+        Entry[] current = array;
+
+        for (Entry curr: current
+             ) {
+            if(curr==null) {
+                head += "null, ";
+            }
+            while (curr!=null){
+                head +="(" + curr.key+", "+ curr.value+")"+", ";
+                curr=curr.next;
+
+            }
+
+        }
+
+
+
+        return  head +']'+'}';
+    }
+}
